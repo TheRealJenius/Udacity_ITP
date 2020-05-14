63e47875c9037d574a9d0b63270ff79406d921b5
Jesse Appiah <TheRealJenius@gmail.com>
2020-05-14 21:34:00 +0100

    Learning about functions, function expressions, named function expressions and inline functions

---
+++ JavaScript/HappyLaugh.js
@@ -0,0 +1,56 @@
+/*
+ * Programming Quiz: Laugh (5-4)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `laugh`
+ * - Your code should include an anonymous function expression stored in the variable `laugh`
+ * - Your anonymous function expression should take one argument
+ * - Your anonymous function expression should return the correct number of `hahaha`\'s
+ */
+
+
+var laugh = function (count){
+    var ha = "";
+    for(var i = 0; i<count; i++){
+        ha += "ha";
+    }
+    return ha+"!";
+}/* finish the function expression */
+
+console.log(laugh(10));
+
+
+
+/*
+ * Programming Quiz: Inline Functions (5-6)
+ */
+ 
+ /*
+ * QUIZ REQUIREMENTS
+ * - Your code should have an `emotions()` function
+ * - Your code should call the `emotions()` function
+ * - Your `emotions()` function call should have an inline function expression passed as the second parameter
+ * - Your function expression should return the expected output
+ */
+
+
+// don't change this code
+// emotions() function definition
+function emotions(myString, myFunc) {
+    console.log("I am " + myString + ", " + myFunc(2));
+}
+
+// Call the emotions() function with two arguments
+// Argument 1 - "happy" string
+// Argument 2 - an inline function expression
+
+emotions("happy", function laugh(count){
+    var ha = "";
+    for(var i = 0; i<count; i++){
+        ha += "ha";
+    }
+    return ha+"!";
+}
+);
